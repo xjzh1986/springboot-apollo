d92fc250f51ae25d848aab8d70af491d6eae2732
xjzh1986 <273904882@163.com>
2020-01-05 16:56:33 +0800

    apollo 代码提交

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,19 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    String port;
+    @Value( "${server.url}" )
+    String url;
+
+    @GetMapping("index")
+    public String index() {
+        System.out.println("url === "+url);
+        return "success";
+    }
+}
