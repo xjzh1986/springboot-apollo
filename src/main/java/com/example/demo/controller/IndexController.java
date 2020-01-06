e56e8729dbe82e7b4363583a0323c8b182d3a09f
xjzh1986 <273904882@163.com>
2020-01-06 09:08:12 +0800

    apollo 代码提交

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -14,6 +14,6 @@ public class IndexController {
     @GetMapping("index")
     public String index() {
         System.out.println("url === "+url);
-        return "success";
+        return url;
     }
 }
